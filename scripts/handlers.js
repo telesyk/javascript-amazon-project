943705381555df97fe7d58278a9cda331a1bcd8b
telesyk <telesyk89@gmail.com>
2025-02-15 14:00:12 +0100

    Add new action and handler to updateCheckoutItemQuantity.
    Extend constants with SELECTOR_QUANTITY_LABEL, SELECTOR_QUANTITY_CONTROL, EVENT_UPDATE_CHECKOUT_ITEM_QUANTITY
    Add new handleCheckoutItemQuantity, update handleUpdateQuantity
    Update render selectHTML, deliveryOptionHTML, checkoutItem
    Update styles for quantity-control and quantity-label

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -11,8 +11,12 @@ import {
 import { 
   ATTRIBUTE_DATA_CONTROL,
   EVENT_ADD_TO_CART,
+  EVENT_UPDATE_CHECKOUT_ITEM_QUANTITY,
   SELECTOR_CHECKOUT_DELIVERY_DATE,
+  SELECTOR_IS_VISIBLE,
   SELECTOR_PAYMENT_SUMMARY,
+  SELECTOR_QUANTITY_CONTROL,
+  SELECTOR_QUANTITY_LABEL,
 } from "./constants.js";
 
 export function handleAddToCartEvent(target) {
@@ -32,12 +36,17 @@ export function handleAddToCartEvent(target) {
   if (currentProductData) updateAddedMessage(elementProduct); // display message/alert "Product added"
 }
 
-export function handleChangeQuantity(target) {
+export function handleUpdateQuantity(target) {
   const elementCardContainer = document.getElementById(target.dataset.productId);
-  const buttonSelector = `[${ATTRIBUTE_DATA_CONTROL}=${EVENT_ADD_TO_CART}]`;
-  const elementAddButton = elementCardContainer.querySelector(buttonSelector);
-  
-  elementAddButton.dataset.productQuantity = target.value;
+  const buttons = elementCardContainer.querySelectorAll(`[${ATTRIBUTE_DATA_CONTROL}]`);
+  buttons.forEach(btn => {
+    if (
+      btn.matches(`[${ATTRIBUTE_DATA_CONTROL}=${EVENT_ADD_TO_CART}]`) ||
+      btn.matches(`[${ATTRIBUTE_DATA_CONTROL}=${EVENT_UPDATE_CHECKOUT_ITEM_QUANTITY}]`)
+    ) {
+      btn.dataset.productQuantity = target.value;
+    }
+  });
 }
 
 export function handleChangeDeliveryOption(target) {
@@ -63,8 +72,7 @@ export function handleChangeDeliveryOption(target) {
 }
 
 export function handleRemoveFromCart(target) {
-  const parentContainer = target.closest('[id]'); // SHOULD be rewrited with more conventioned style
-  const productId = parentContainer.id;
+  const productId = target.dataset.productId;
   const currentCheckoutState = updateCheckoutState();
   const currentGeneralState = updateGeneralState();
   const removedProduct = currentCheckoutState.filter(product => product.id === productId)[0];
@@ -80,4 +88,40 @@ export function handleRemoveFromCart(target) {
   updateGeneralState(newGeneralState);
 
   renderCheckout(newCartState);
+}
+
+export function handleCheckoutItemQuantity(target) {
+  const parentContainer = document.getElementById(target.dataset.productId)
+  const elementQuantityControl = parentContainer.querySelector(SELECTOR_QUANTITY_CONTROL);
+  const elementQuantityLabel = parentContainer.querySelector(SELECTOR_QUANTITY_LABEL);
+  const elementPaymentSummaryContainer = document.querySelector(SELECTOR_PAYMENT_SUMMARY);
+  const currentCheckoutState = updateCheckoutState();
+  const currentGeneralState = updateGeneralState();
+  const newProductState = (product) => {
+    return {
+      ...product,
+      quantity: Number(target.dataset.productQuantity),
+      stock: (product.quantity + product.stock) - Number(target.dataset.productQuantity),
+    };
+  };
+  
+  target.innerText = target.innerText !== 'Save' ? 'Save' : 'Update';
+  elementQuantityControl.classList.toggle(SELECTOR_IS_VISIBLE);
+  elementQuantityLabel.classList.toggle(SELECTOR_IS_VISIBLE);
+  elementQuantityLabel.innerHTML = target.dataset.productQuantity;
+  
+  const newCheckoutState = currentCheckoutState.map(product => {
+    return product.id === target.dataset.productId ? newProductState(product) : product;
+  });
+  const newGeneralState = currentGeneralState.map(product => {
+    return product.id === target.dataset.productId ? newProductState(product) : product;
+  });
+
+  updateCheckoutState(newCheckoutState);
+  updateGeneralState(newGeneralState);
+
+  const checkoutPrices = getCheckoutPrices();
+  const elementNewPaymentSummary = renderPaymentSummary(checkoutPrices);
+
+  elementPaymentSummaryContainer.innerHTML = elementNewPaymentSummary.innerHTML;
 }
--- scripts/render.js
+++ scripts/render.js
@@ -22,6 +22,7 @@ import {
   SELECTOR_PAYMENT_SUMMARY,
   ATTRIBUTE_DELIVERY_PRICE,
   EVENT_REMOVE_FROM_CART,
+  EVENT_UPDATE_CHECKOUT_ITEM_QUANTITY,
 } from "./constants.js";
 import { deliveryOptions } from "../data/delivery-options.js";
 
@@ -31,22 +32,24 @@ function renderQuantityStringHTML(quantity) {
   return `<div class="product-quantity-left">Only <b>${quantity}</b> left</div>`;
 }
 
-function renderSelectHTML({id, stock}) {
+function renderSelectHTML({id, stock, attrString}) {
   if (!stock || !id) return '';
 
   const optionsList = createIntArray(stock);
+  const optionsHTMLString = optionsList.map((value, index) => {
+    if (index === 0) {
+      return `<option selected value="${value}">${value}</option>`;
+    }
+    return `<option value="${value}">${value}</option>`;
+  }).join('');
 
   return `
     <select 
+      ${!attrString  ? '' : attrString}
       ${ATTRIBUTE_DATA_CONTROL}=${EVENT_SET_ITEM_QUANTITY} 
       ${ATTRIBUTE_DATA_PRODUCT_ID}=${id}
     >
-      ${optionsList.map((value, index) => {
-        if (index === 0) {
-          return `<option selected value="${value}">${value}</option>`;
-        }
-        return `<option value="${value}">${value}</option>`;
-      })}
+      ${optionsHTMLString}
     </select>
   `;
 }
@@ -160,8 +163,6 @@ function renderDeliveryOptionHTML({
 }) {
   const deliveryDateString = getFormatedDateString( getNextDate(dateValue) );
   const attributeDeliveryDate = `${ATTRIBUTE_DELIVERY_DATE}="${deliveryDateString}"`;
-  const attributeDeliveryPrice = `${ATTRIBUTE_DELIVERY_PRICE}="${price}"`;
-  const attributeDataControl = `${ATTRIBUTE_DATA_CONTROL}="${EVENT_CHANGE_DELIVERY_OPTION}"`;
 
   return `
     <label class="delivery-option">
@@ -169,8 +170,8 @@ function renderDeliveryOptionHTML({
         class="delivery-option-input"
         type="radio"
         name="delivery-option-${index}"
-        ${attributeDeliveryPrice}
-        ${attributeDataControl}
+        ${ATTRIBUTE_DELIVERY_PRICE}=${price}
+        ${ATTRIBUTE_DATA_CONTROL}=${EVENT_CHANGE_DELIVERY_OPTION}
         ${!dateValue ? '' : attributeDeliveryDate}
         ${shippingPrice !== price ? '' : 'checked'} 
       />
@@ -239,6 +240,7 @@ function renderCheckoutItem({
   name,
   index,
   shippingPrice,
+  stock,
 }) {
   const dollarPrice = convertCentToDollar(priceCents);
   const deliveryOptionsHTML = deliveryOptions.reduce((html, option) => {
@@ -247,7 +249,8 @@ function renderCheckoutItem({
   }, '');
   const deliveryOptionChecked = deliveryOptions.filter(option => option.price === shippingPrice)[0];
   const deliveryDateString = getFormatedDateString( getNextDate(deliveryOptionChecked.dateValue) );
-  const attributeDeleteDataControl = `${ATTRIBUTE_DATA_CONTROL}="${EVENT_REMOVE_FROM_CART}"`;
+  const selectAttrString = 'class="quantity-control js-quantity-control"';
+  const selectHTML = renderSelectHTML({id, stock: stock + quantity, attrString: selectAttrString});
   const template = `
     <div class="cart-item-container" id="${id}">
       <div class="delivery-date">
@@ -261,17 +264,20 @@ function renderCheckoutItem({
           <div class="product-price">$${dollarPrice}</div>
           <div class="product-quantity">
             <span>
-              Quantity: <span class="quantity-label">${quantity}</span>
+              Quantity: <span class="quantity-label is-visible js-quantity-label">${quantity}</span>
+              ${selectHTML}
             </span>
-            <button class="update-quantity-link link-primary">
-              Update
-            </button>
+            <button 
+              class="update-quantity-link link-primary" 
+              ${ATTRIBUTE_DATA_CONTROL}=${EVENT_UPDATE_CHECKOUT_ITEM_QUANTITY}
+              ${ATTRIBUTE_DATA_PRODUCT_QUANTITY}=${quantity} 
+              ${ATTRIBUTE_DATA_PRODUCT_ID}=${id}
+            >Update</button>
             <button 
               class="delete-quantity-link link-primary" 
-              ${attributeDeleteDataControl}
-            >
-              Delete
-            </button>
+              ${ATTRIBUTE_DATA_CONTROL}=${EVENT_REMOVE_FROM_CART}
+              ${ATTRIBUTE_DATA_PRODUCT_ID}=${id}
+            >Delete</button>
           </div>
         </div>
 
